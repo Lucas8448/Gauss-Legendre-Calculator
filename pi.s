4297c12c739ad46b99e9987b5f329ad2a21163d1
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-07-27 22:08:35 +0200

    Complete calculation logic

--- pi.s
+++ pi.s
@@ -3,10 +3,34 @@
     .p2align 2
 _main:
     LDR     D0, =const_one
+    LDR     D1, =const_two
     FSQRT   D1, D1
     FDIV    D1, D0, D1
     LDR     D2, =const_quarter
     LDR     D3, =const_one
+
+    FADD    D4, D0, D1
+    LDR     D5, =const_two
+    FDIV    D4, D4, D5
+    FMUL    D5, D0, D1
+    FSQRT   D5, D5
+    FSUB    D6, D0, D4
+    FMUL    D6, D6, D6
+    FMUL    D6, D6, D3
+    FSUB    D2, D2, D6
+    FMUL    D3, D3, D5
+
+    FADD    D7, D4, D5
+    FMUL    D7, D7, D7
+    LDR     D8, =const_four
+    FMUL    D8, D8, D2
+    FDIV    D7, D7, D8
+
+    ADRP    X0, fmt@PAGE
+    ADD     X0, X0, fmt@PAGEOFF
+    FMOV    V0, D7
+    BL      _printf
+
     MOV     W0, #0
     RET
 
