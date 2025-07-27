ee8e2b892247010b9431fd472f5852bdeac6fddf
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-07-27 22:35:21 +0200

    Add iterations to show progress in the Gauss-Legendre calculation.

--- pi.s
+++ pi.s
@@ -2,9 +2,9 @@
     .globl _main
     .p2align 2
 _main:
-    SUB     SP, SP, #16
-    STP     X29, X30, [SP]
-    MOV     X29, SP
+    SUB     SP, SP, #32
+    STP     X29, X30, [SP, #16]
+    ADD     X29, SP, #16
     
     FMOV    D0, #1.0
     FMOV    D1, #2.0
@@ -12,6 +12,12 @@ _main:
     FDIV    D1, D0, D1
     FMOV    D2, #0.25
     FMOV    D3, #1.0
+    
+    MOV     W19, #1
+
+iteration_loop:
+    CMP     W19, #10
+    B.GT    end_program
 
     FADD    D4, D0, D1
     FMOV    D5, #2.0
@@ -21,25 +27,37 @@ _main:
     FSUB    D6, D0, D4
     FMUL    D6, D6, D6
     FMUL    D6, D6, D3
-    FSUB    D2, D2, D6
+    FSUB    D8, D2, D6
+    FADD    D9, D3, D3
 
     FADD    D7, D4, D5
     FMUL    D7, D7, D7
-    FMOV    D8, #4.0
-    FMUL    D8, D8, D2
-    FDIV    D7, D7, D8
+    FMOV    D10, #4.0
+    FMUL    D10, D10, D8
+    FDIV    D7, D7, D10
 
+    STR     W19, [SP]
     MOV     X8, SP
+    ADD     X8, X8, #8
     STR     D7, [X8]
     ADRP    X0, fmt@PAGE
     ADD     X0, X0, fmt@PAGEOFF
     BL      _printf
 
-    LDP     X29, X30, [SP]
-    ADD     SP, SP, #16
+    FMOV    D0, D4
+    FMOV    D1, D5
+    FMOV    D2, D8
+    FMOV    D3, D9
+    ADD     W19, W19, #1
+    B       iteration_loop
+
+end_program:
+
+    LDP     X29, X30, [SP, #16]
+    ADD     SP, SP, #32
     MOV     W0, #0
     RET
 
     .section __DATA,__data
 fmt:
-    .asciz "pi = %.12f\n"
+    .asciz "Iteration %d: pi = %.15f\n"
